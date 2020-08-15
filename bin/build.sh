72fe865c02f613338c92ef7c8a6bbcb95a8b9863
Rui Marinho <ruipmarinho@gmail.com>
2020-08-16 00:46:42 +0100

    Replace online google closure compiler by local installation

--- bin/build.sh
+++ bin/build.sh
@@ -1,41 +1,57 @@
 #!/bin/bash
 
-PATH=./node_modules/.bin/:$PATH
+rm -rf dist/* \
+&& echo "Compiling using Google Closure Compiler..." \
+&& ./node_modules/.bin/google-closure-compiler \
+--compilation_level=SIMPLE \
+--js=node_modules/google-closure-library/closure/goog/array/array.js \
+--js=node_modules/google-closure-library/closure/goog/asserts/asserts.js \
+--js=node_modules/google-closure-library/closure/goog/base.js \
+--js=node_modules/google-closure-library/closure/goog/debug/error.js \
+--js=node_modules/google-closure-library/closure/goog/dom/asserts.js \
+--js=node_modules/google-closure-library/closure/goog/dom/htmlelement.js \
+--js=node_modules/google-closure-library/closure/goog/dom/nodetype.js \
+--js=node_modules/google-closure-library/closure/goog/dom/safe.js \
+--js=node_modules/google-closure-library/closure/goog/dom/tagname.js \
+--js=node_modules/google-closure-library/closure/goog/dom/tags.js \
+--js=node_modules/google-closure-library/closure/goog/fs/blob.js \
+--js=node_modules/google-closure-library/closure/goog/fs/url.js \
+--js=node_modules/google-closure-library/closure/goog/functions/functions.js \
+--js=node_modules/google-closure-library/closure/goog/html/safehtml.js \
+--js=node_modules/google-closure-library/closure/goog/html/safescript.js \
+--js=node_modules/google-closure-library/closure/goog/html/safestyle.js \
+--js=node_modules/google-closure-library/closure/goog/html/safestylesheet.js \
+--js=node_modules/google-closure-library/closure/goog/html/safeurl.js \
+--js=node_modules/google-closure-library/closure/goog/html/trustedresourceurl.js \
+--js=node_modules/google-closure-library/closure/goog/html/trustedtypes.js \
+--js=node_modules/google-closure-library/closure/goog/html/uncheckedconversions.js \
+--js=node_modules/google-closure-library/closure/goog/i18n/bidi.js \
+--js=node_modules/google-closure-library/closure/goog/labs/useragent/browser.js \
+--js=node_modules/google-closure-library/closure/goog/labs/useragent/util.js \
+--js=node_modules/google-closure-library/closure/goog/object/object.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/descriptor.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/fielddescriptor.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/lazydeserializer.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/message.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/pbliteserializer.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/serializer.js \
+--js=node_modules/google-closure-library/closure/goog/string/const.js \
+--js=node_modules/google-closure-library/closure/goog/string/internal.js \
+--js=node_modules/google-closure-library/closure/goog/string/string.js \
+--js=node_modules/google-closure-library/closure/goog/string/stringbuffer.js \
+--js=node_modules/google-closure-library/closure/goog/string/typedstring.js \
+--js=src/index.js \
+--js=src/metadata.js \
+--js=src/phonemetadata.pb.js \
+--js=src/phonenumber.pb.js \
+--js=src/phonenumberutil.js \
+--js=src/shortnumberinfo.js \
+--js=src/shortnumbermetadata.js \
+--js=src/asyoutypeformatter.js \
+--js_output_file=dist/libphonenumber.original.js \
+&& ./node_modules/.bin/browserify dist/libphonenumber.original.js --standalone libphonenumber --no-browser-field --outfile dist/libphonenumber.js \
+&& rm dist/libphonenumber.original.js \
+&& echo "Build completed!"
 
-# Clean previous distribution build.
-rm -rf dist/*
 
-# Test if online compilation should be used.
-if [ "${ONLINE:-true}" == "true" ]; then
-  echo "Compiling using Google Closure Service..."
 
-  curl --silent \
-    --data output_format=text \
-    --data output_info=compiled_code \
-    --data use_closure_library=true \
-    --data compilation_level=SIMPLE_OPTIMIZATIONS \
-    --data formatting=PRETTY_PRINT \
-    --data-urlencode js_code@src/asyoutypeformatter.js \
-    --data-urlencode js_code@src/index.js \
-    --data-urlencode js_code@src/metadata.js \
-    --data-urlencode js_code@src/phonemetadata.pb.js \
-    --data-urlencode js_code@src/phonenumber.pb.js \
-    --data-urlencode js_code@src/phonenumberutil.js \
-    --data-urlencode js_code@src/shortnumberinfo.js \
-    --data-urlencode js_code@src/shortnumbermetadata.js \
-    --output dist/libphonenumber.original.js \
-    https://closure-compiler.appspot.com/compile
-else
-  echo "Compiling locally..."
-
-  ant build
-fi
-
-if [ "${BROWSERIFY:-true}" == "true" ]; then
-  echo "Browserifying..."
-
-  browserify dist/libphonenumber.original.js --standalone libphonenumber --no-browser-field --outfile dist/libphonenumber.js
-  rm dist/libphonenumber.original.js
-fi
-
-echo "Build completed!"
